5917c4e85d2bff47b5f8679f76e7b4d9e30cac2b
Josip Domsic <josip.domsic@gmail.com>
2016-10-20 20:11:11 +0200

    Added helper functions

--- rpmextract.py
+++ rpmextract.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python3
 
 import argparse
+import copy
+import gzip
+import io
+import struct
+import sys
 
+
+def c2i(char):
+    return struct.unpack('>b', char)[0]
+
+def s2i(short):
+    return struct.unpack('>h', short)[0]
+
+def i2i(unsigned_int):
+    return struct.unpack('>I', unsigned_int)[0]
+
+def str_from_bytes(bytes, count):
+    strings = bytes.split(b'\x00')
+
+    return strings[0:count]
 
 def main():
     settings = parse_arguments()
+
+
+def die(message):
+    print (message, file=sys.stderr)
+    exit(-1)
 
 
 def parse_arguments():
